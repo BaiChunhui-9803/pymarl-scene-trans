938a8b7d512efccf14ec64920c5b2ec1fb46e8dc
binich-9803 <bch980311@163.com>
2024-10-25 17:20:54 +0800

    REGISTRY custom_controller.

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -3,3 +3,7 @@ REGISTRY = {}
 from .basic_controller import BasicMAC
 
 REGISTRY["basic_mac"] = BasicMAC
+
+# binich
+from .custom_controller import CustomController
+REGISTRY["custom_controller"] = CustomController
