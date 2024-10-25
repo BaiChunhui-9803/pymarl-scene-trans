206b43508a9b4638c379f7f841fda023e207a2df
binich-9803 <bch980311@163.com>
2024-10-25 17:23:05 +0800

    Modified custom_episode_buffer.py.

--- src/components/custom_episode_buffer.py
+++ src/components/custom_episode_buffer.py
@@ -27,51 +27,18 @@ class CustomEpisodeCBSBatch:
             self._setup_data(self.scheme, self.groups, batch_size, max_seq_length, self.preprocess)
 
     def _setup_data(self, scheme, groups, batch_size, max_seq_length, preprocess):
-        if preprocess is not None:
-            for k in preprocess:
-                assert k in scheme
-                new_k = preprocess[k][0]
-                transforms = preprocess[k][1]
-
-                vshape = self.scheme[k]["vshape"]
-                dtype = self.scheme[k]["dtype"]
-                for transform in transforms:
-                    vshape, dtype = transform.infer_output_info(vshape, dtype)
-
-                self.scheme[new_k] = {
-                    "vshape": vshape,
-                    "dtype": dtype
-                }
-                if "group" in self.scheme[k]:
-                    self.scheme[new_k]["group"] = self.scheme[k]["group"]
-                if "episode_const" in self.scheme[k]:
-                    self.scheme[new_k]["episode_const"] = self.scheme[k]["episode_const"]
-
         assert "filled" not in scheme, '"filled" is a reserved key for masking.'
         scheme.update({
-            "filled": {"vshape": (1,), "dtype": th.long},
+            "filled": {},
         })
 
         for field_key, field_info in scheme.items():
-            assert "vshape" in field_info, "Scheme must define vshape for {}".format(field_key)
-            vshape = field_info["vshape"]
             episode_const = field_info.get("episode_const", False)
-            group = field_info.get("group", None)
-            dtype = field_info.get("dtype", th.float32)
-
-            if isinstance(vshape, int):
-                vshape = (vshape,)
-
-            if group:
-                assert group in groups, "Group {} must have its number of members defined in _groups_".format(group)
-                shape = (groups[group], *vshape)
-            else:
-                shape = vshape
 
             if episode_const:
-                self.data.episode_data[field_key] = th.zeros((batch_size, *shape), dtype=dtype, device=self.device)
+                self.data.episode_data[field_key] = [[None for _ in range(max_seq_length)] for _ in range(batch_size)]
             else:
-                self.data.transition_data[field_key] = th.zeros((batch_size, max_seq_length, *shape), dtype=dtype, device=self.device)
+                self.data.transition_data[field_key] = [[None for _ in range(max_seq_length)] for _ in range(batch_size)]
 
     def extend(self, scheme, groups=None):
         self._setup_data(scheme, self.groups if groups is None else groups, self.batch_size, self.max_seq_length)
@@ -83,32 +50,29 @@ class CustomEpisodeCBSBatch:
             self.data.episode_data[k] = v.to(device)
         self.device = device
 
-    def update(self, data, bs=slice(None), ts=slice(None), mark_filled=True):
-        slices = self._parse_slices((bs, ts))
+    def update(self, data, bs=0, ts=0, mark_filled=True):
+        batch_id = bs
+        t_id = ts
         for k, v in data.items():
             if k in self.data.transition_data:
                 target = self.data.transition_data
                 if mark_filled:
-                    target["filled"][slices] = 1
+                    target["filled"][batch_id][t_id] = 1
                     mark_filled = False
-                _slices = slices
             elif k in self.data.episode_data:
                 target = self.data.episode_data
-                _slices = slices[0]
             else:
                 raise KeyError("{} not found in transition or episode data".format(k))
 
-            dtype = self.scheme[k].get("dtype", th.float32)
-            v = th.tensor(v, dtype=dtype, device=self.device)
-            self._check_safe_view(v, target[k][_slices])
-            target[k][_slices] = v.view_as(target[k][_slices])
+            target[k][batch_id][t_id] = v
+        print(self.data.transition_data)
 
-            if k in self.preprocess:
-                new_k = self.preprocess[k][0]
-                v = target[k][_slices]
-                for transform in self.preprocess[k][1]:
-                    v = transform.transform(v)
-                target[new_k][_slices] = v.view_as(target[new_k][_slices])
+            # if k in self.preprocess:
+            #     new_k = self.preprocess[k][0]
+            #     v = target[k][_slices]
+            #     for transform in self.preprocess[k][1]:
+            #         v = transform.transform(v)
+            #     target[new_k][_slices] = v.view_as(target[new_k][_slices])
 
     def _check_safe_view(self, v, dest):
         idx = len(v.shape) - 1
@@ -203,9 +167,9 @@ class CustomEpisodeCBSBatch:
                                                                                      self.groups.keys())
 
 
-class ReplayBuffer(CustomEpisodeCBSBatch):
+class CustomReplayBuffer(CustomEpisodeCBSBatch):
     def __init__(self, scheme, groups, buffer_size, max_seq_length, preprocess=None, device="cpu"):
-        super(ReplayBuffer, self).__init__(scheme, groups, buffer_size, max_seq_length, preprocess=preprocess, device=device)
+        super(CustomReplayBuffer, self).__init__(scheme, groups, buffer_size, max_seq_length, preprocess=preprocess, device=device)
         self.buffer_size = buffer_size  # same as self.batch_size but more explicit
         self.buffer_index = 0
         self.episodes_in_buffer = 0
